52be4f00002604c1dd0ab9b42ccacb3b81ec15d9
hissy <hishikawa@concrete5.co.jp>
2013-12-17 03:57:39 +0900

    space tweak

--- packages/set_loggedin_cookie/controller.php
+++ packages/set_loggedin_cookie/controller.php
@@ -3,18 +3,18 @@
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
 class SetLoggedinCookiePackage extends Package {
-   
-    protected $pkgHandle = 'set_loggedin_cookie';
-    protected $appVersionRequired = '5.6.2';
-    protected $pkgVersion = '0.0.1';
-
-    public function getPackageDescription() {
-        return t('Set additional cookie when user is logged in, for reverse proxy.');
-    }
-
-    public function getPackageName() {
-        return t('Set LoggedIn Cookie');
-    }
+	
+	protected $pkgHandle = 'set_loggedin_cookie';
+	protected $appVersionRequired = '5.6.2';
+	protected $pkgVersion = '0.0.1';
+	
+	public function getPackageDescription() {
+		return t('Set additional cookie when user is logged in, for reverse proxy.');
+	}
+	
+	public function getPackageName() {
+		return t('Set LoggedIn Cookie');
+	}
 	
 	public function on_start() {
 		Events::extend('on_before_render', __CLASS__, 'addLoggedInCookie', __FILE__);
